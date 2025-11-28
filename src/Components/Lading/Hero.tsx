a7777d31813728184e1a5e2f457e881527c5d021
Your Name <you@example.com>
2025-11-28 13:22:30 -0300

    git commit -m "Show 'Comenzar' button only for unauthenticated users in Hero component"

--- src/Components/Lading/Hero.tsx
+++ src/Components/Lading/Hero.tsx
@@ -2,8 +2,12 @@ import styles from '../../Assets/Style/Home/Hero.module.css'
 import { IconSparkles } from  '../../Assets/icons/IconSparkles'
 import { IconBolt } from '../../Assets/icons/IconBolt'
 import { IconHeart } from '../../Assets/icons/IconHeart'
+import { useAuth } from '../../context/AuthContext'
 
 export const Hero = () => {
+
+  const { user } = useAuth()
+
   return (
     <section className={styles.Hero}>
       <h1 className={styles.heroTitle}>
@@ -11,8 +15,10 @@ export const Hero = () => {
       </h1>
       <p className={styles.heroSubtitle}>Descubre, vota y conecta con la comunidad anime argentina. Sigue tus <br />series, comparte opiniones y encuentra tu próximo anime favorito.</p>
       <div className={styles.ctaContainer}>
-        <a href="#get-started" className={`${styles.ctaButton} ${styles.ctaButtonPrimary}`}> <IconBolt size={20} color='white' />Comenzar</a>
-        <a href="#get-started" className={`${styles.ctaButton} ${styles.ctaButtonSecondary}`}> <IconHeart size={20} />Explorar Catálogo</a>
+        {
+          !user && <a href="/register" className={`${styles.ctaButton} ${styles.ctaButtonPrimary}`}> <IconBolt size={20} color='white' />Comenzar</a>
+        }
+        <a href="/home" className={`${styles.ctaButton} ${styles.ctaButtonSecondary}`}> <IconHeart size={20} />Explorar Catálogo</a>
       </div>
     </section>
   )
