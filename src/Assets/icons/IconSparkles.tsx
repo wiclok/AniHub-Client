178922494cf015d8219f05f1983dbb88e1c3c8d0
Your Name <you@example.com>
2025-11-28 17:30:50 -0300

    git commit -m "Personalize Hero greeting with user first name and make IconSparkles stylable via className"

--- src/Assets/icons/IconSparkles.tsx
+++ src/Assets/icons/IconSparkles.tsx
@@ -1,4 +1,8 @@
-export const IconSparkles = () => {
+type typeIconSparkles = {
+  className?: string
+}
+
+export const IconSparkles = ({className = ''}: typeIconSparkles ) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -10,7 +14,7 @@ export const IconSparkles = () => {
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
-      className="icon icon-tabler icons-tabler-outline icon-tabler-sparkles"
+      className={`icon icon-tabler icons-tabler-outline icon-tabler-sparkles ${className}`}
     >
       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
       <path d="M16 18a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm0 -12a2 2 0 0 1 2 2a2 2 0 0 1 2 -2a2 2 0 0 1 -2 -2a2 2 0 0 1 -2 2zm-7 12a6 6 0 0 1 6 -6a6 6 0 0 1 -6 -6a6 6 0 0 1 -6 6a6 6 0 0 1 6 6z" />
